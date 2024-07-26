9008e65b636c5e872b3c20055ce715611c03ebe6
VictorAkashReznov <165830558+VictorAkashReznov@users.noreply.github.com>
2024-07-26 19:39:29 +0530

    Add files via upload

---
+++ MiddleOfLinkList.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PII pair<int, int>
+#define VI vector<int>
+#define MAX_SET set<int, greater<int>> set_name
+#define PQ priority_queue<int>
+#define MIN_PQ priority_queue<int, vector<int>, greater<int>>
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while (fast != NULL && fast->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        if (fast == NULL)
+        {
+            return slow;
+        }
+        else
+        {
+            return slow->next;
+        }
+    }
+};
+int main()
+{
+
+    return 0;
+}
